eb4ee0a1fdd9fa6d2464b7d6c37d4c2f6c42c3b7
Rhetticle <rhett.humphreys1@gmail.com>
2025-06-18 14:31:35 +1000

    Add LVGL task to dgas_ui.c

--- core/ui/dgas_ui.c
+++ core/ui/dgas_ui.c
@@ -8,9 +8,14 @@
 #include <dgas_types.h>
 #include <dgas_ui.h>
 #include <display.h>
+#include <lvgl.h>
 
-// Stores handle of task
+// Stores handle of UI controller task
 static TaskHandle_t taskHandleDgasUi;
+// Stores handle of LVGL task
+static TaskHandle_t taskHandleLVGL;
+// semaphore to synchronise GUI operations
+SemaphoreHandle_t semaphoreUI;
 
 /**
  * Get task handle of DGAS UI task
@@ -22,15 +27,62 @@ TaskHandle_t task_dgas_ui_get_handle(void) {
 }
 
 /**
+ * Get task handle of LVGL task
+ *
+ * Return: Task handle of LVGL task
+ * */
+TaskHandle_t task_lvgl_get_handle(void) {
+	return taskHandleLVGL;
+}
+
+/**
+ * Take UI semaphore
+ *
+ * Return: pdTRUE if taken, pdFALSE otherwise
+ * */
+BaseType_t ui_take_semaphore(void) {
+	return xSemaphoreTake(semaphoreUI, 10);
+}
+
+/**
+ * Give UI semaphore
+ *
+ * Return: None
+ * */
+void ui_give_semaphore(void) {
+	xSemaphoreGive(semaphoreUI);
+}
+
+/**
  * Thread function for DGAS UI task
  *
  * Return: None
  * */
 void task_dgas_ui(void) {
+	taskENTER_CRITICAL();
+	semaphoreUI = xSemaphoreCreateBinary();
 	display_init();
+	taskEXIT_CRITICAL();
 
 	for(;;) {
 
+	}
+}
+
+/**
+ * LVGL tick task function.
+ *
+ * Return: None
+ * */
+void task_lvgl_tick(void) {
+
+	for(;;) {
+		if (ui_take_semaphore() == pdTRUE) {
+			// got semaphore, update UI
+			lv_task_handler();
+			ui_give_semaphore();
+		}
+		vTaskDelay(10);
 	}
 }
 
@@ -40,6 +92,10 @@ void task_dgas_ui(void) {
  * Return: None
  * */
 void task_dgas_ui_init(void) {
+	// create UI controller task
 	xTaskCreate((void*) &task_dgas_ui, "TaskDgasUi", TASK_DGAS_UI_STACK_SIZE,
 			NULL, TASK_DGAS_UI_PRIORITY, &taskHandleDgasUi);
+	// create LVGL tick task
+	xTaskCreate((void*) &task_lvgl_tick, "TaskLVGL", TASK_DGAS_LVGL_STACK_SIZE,
+			NULL, TASK_DGAS_LVGL_PRIORITY, &taskHandleLVGL);
 }
--- include/dgas_ui.h
+++ include/dgas_ui.h
@@ -9,17 +9,20 @@
 #define INC_DGAS_UI_H_
 
 #include <dgas_types.h>
+#include <lvgl.h>
 
 #define TASK_DGAS_UI_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
 #define TASK_DGAS_UI_PRIORITY (tskIDLE_PRIORITY + 3)
+#define TASK_DGAS_LVGL_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
+#define TASK_DGAS_LVGL_PRIORITY (tskIDLE_PRIORITY + 4)
 
-/*
+
 typedef struct {
-	lv_screen_t* scrn;
+	lv_obj_t* screen;
 	lv_group_t* group;
 	lv_obj_t** objs;
 	void (*evt_handler)(lv_event_t*);
 }Screen;
-*/
+
 
 #endif /* INC_DGAS_UI_H_ */
